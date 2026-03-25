cf611395d7a66cee0147ea5d455a00cb8e73b142
neethu2026 <neethunayanam@gmail.com>
2026-03-25 16:33:58 +0530

    Update today_delivery_view.php

--- views/agency/routes/today_delivery_view.php
+++ views/agency/routes/today_delivery_view.php
@@ -166,6 +166,7 @@ $cardColors = [
 "bg-green-100 border-green-300 text-green-800",
 "bg-purple-100 border-purple-300 text-purple-800",
 "bg-orange-100 border-orange-300 text-orange-800",
+"bg-red-100 border-red-300 text-red-800",
 "bg-pink-100 border-pink-300 text-pink-800"
 ];
 
@@ -227,18 +228,19 @@ $totalCancelledPackets += $cancelledQty;
 <div class="text-lg font-bold"><?= $totalPackets ?></div>
 </div>
 
-<div class="mt-4 grid grid-cols-3 gap-2 text-center text-xs font-semibold">
-    <div class="bg-green-100 text-green-700 p-2 rounded">
-        Delivered <br><?= $deliveredCount ?>
-    </div>
+<div class="border rounded-lg px-3 py-2 bg-green-100">
+<div class="text-xs">Delivered</div>
+<div class="text-lg font-bold"><?= $deliveredCount ?></div>
+</div>
 
-    <div class="bg-red-100 text-red-700 p-2 rounded">
-        Not Delivered <br><?= $notDeliveredCount ?>
-    </div>
+   <div class="border rounded-lg px-3 py-2 bg-red-100">
+<div class="text-xs">Not Delivered</div>
+<div class="text-lg font-bold"><?= $notDeliveredCount ?></div>
+</div>
 
-    <div class="bg-yellow-100 text-yellow-700 p-2 rounded">
-        Pending <br><?= $pendingCount ?>
-    </div>
+   <div class="border rounded-lg px-3 py-2 bg-orange-100">
+<div class="text-xs">Pending</div>
+<div class="text-lg font-bold"><?= $pendingCount ?></div>
 </div>
 
 </div>
