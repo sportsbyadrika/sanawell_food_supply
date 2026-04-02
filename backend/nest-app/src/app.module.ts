e60aa4bec689609a427d10b038c67a1a033b08ca
neethu2026 <neethunayanam@gmail.com>
2026-04-02 12:52:37 +0530

    Added Next.js frontend and NestJS backend

---
+++ backend/nest-app/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '', 
+      database: 'sanawell_delivery', 
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
+})
+export class AppModule {}
