dafe145cc569d965a98049f0d19adb0f8d38190d
neethu2026 <neethunayanam@gmail.com>
2026-04-02 14:52:48 +0530

    Clean modern-stack with frontend and backend only

--- models/UserType.php
+++
@@ -1,9 +0,0 @@
-<?php
-class UserType extends BaseModel
-{
-    public function all(): array
-    {
-        $stmt = $this->db->query('SELECT * FROM user_types ORDER BY id');
-        return $stmt->fetchAll();
-    }
-}
